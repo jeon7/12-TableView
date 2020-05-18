ca5512da51b5c43d82f0a8747bb6838c32e62c42
Gukhwa Jeon <jeon.gukhwa7@gmail.com>
2020-05-18 14:37:45 +0200

    Mission Done

--- 12 TableView/AddViewController.swift
+++ 12 TableView/AddViewController.swift
@@ -8,20 +8,59 @@
 
 import UIKit
 
-class AddViewController: UIViewController {
+class AddViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet var myPickerView: UIPickerView!
+    @IBOutlet var imageViewFromPickerView: UIImageView!
     @IBOutlet var tfAddItem: UITextField!
+    
+    let PICKER_VIEW_COLUMN = 1
+    let PICKER_VIEW_HEIGHT: CGFloat = 55
+    var imageFileName = ["cart.png", "clock.png", "pencil.png"]
+    var selectedRowNumber: Int?
+    var imageArray = [UIImage?]()
     
     override func viewDidLoad() {
         print("AddViewController.viewDidLoad()")
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        for i in 0 ..< imageFileName.count {
+            let image = UIImage(named: imageFileName[i])
+            imageArray.append(image)
+        }
+        imageViewFromPickerView.image = imageArray[0]
+        selectedRowNumber = 0
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return PICKER_VIEW_COLUMN
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return PICKER_VIEW_HEIGHT
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return imageFileName.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let imageView = UIImageView(image: imageArray[row])
+        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        selectedRowNumber = row
+        return imageView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        imageViewFromPickerView.image = imageArray[row]
+        
     }
     
     @IBAction func btnAddItem(_ sender: UIButton) {
         print("AddViewController.btnAddItem(sender:)")
+
+        itemsImageFile.append(imageFileName[selectedRowNumber!])
         items.append(tfAddItem.text!) // 전역변수라 그냥 접근 가능
-        itemsImageFile.append("clock.png")
         
         dump(items)
         dump(itemsImageFile)
@@ -29,7 +68,6 @@ class AddViewController: UIViewController {
         
         // 루트 뷰 컨트롤러로 돌아감
         _ = navigationController?.popViewController(animated: true)
-        
     }
     
     /*
